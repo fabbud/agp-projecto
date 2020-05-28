c47f1c806000efbe51c8ef4022cb68c454f97ce8
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-05-28 18:36:15 +0100

    language selector on header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
+import './i18n';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Suspense fallback={null}>
+        <Header />
+      </Suspense>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,23 +4,28 @@ import {
 } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
+
 import './Header.css';
+import LanguageSelector from '../LanguageSelector/LanguageSelector';
 
 const Header = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <Navbar fixed="top" expand="lg" className="navbar">
+      <Navbar expand="lg" className="navbar">
         <Navbar.Brand href="" className="header-logo" />
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="header-toggler" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <NavDropdown title="Sobre" id="basic-nav-dropdown" class="nav-item">
-              <NavDropdown.Item className="dropdown-item" href="">Associação</NavDropdown.Item>
-              <NavDropdown.Item className="dropdown-item" href="">História do Guidismo</NavDropdown.Item>
-              <NavDropdown.Item className="dropdown-item" href="">Estatutos</NavDropdown.Item>
-              <NavDropdown.Item className="dropdown-item" href="">Associação Mundial</NavDropdown.Item>
+            <NavDropdown title={t('sobre.label')} id="basic-nav-dropdown" class="nav-item">
+              <NavDropdown.Item className="dropdown-item" href="">{t('associacao.label')}</NavDropdown.Item>
+              <NavDropdown.Item className="dropdown-item" href="">{t('historiaGuidismo.label')}</NavDropdown.Item>
+              <NavDropdown.Item className="dropdown-item" href="">{t('estatutos.label')}</NavDropdown.Item>
+              <NavDropdown.Item className="dropdown-item" href="">{t('associacaoMundial.label')}</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Pedagogia" id="basic-nav-dropdown" class="nav-item">
+            <NavDropdown title={t('pedagogia.label')} id="basic-nav-dropdown" class="nav-item">
               <NavDropdown.Item className="dropdown-item" href="">Método Guidista</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">Ramo Avezinha</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">Ramo Aventura</NavDropdown.Item>
@@ -29,25 +34,32 @@ const Header = () => {
               <NavDropdown.Item className="dropdown-item" href="">Dirigente</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">Palavra aos Pais</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Publicações" id="basic-nav-dropdown" class="nav-item">
+            <NavDropdown title={t('publicacoes.label')} id="basic-nav-dropdown" class="nav-item">
               <NavDropdown.Item className="dropdown-item" href="">Notícias</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">Jornal O Trevo</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">Recursos</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Loja" id="basic-nav-dropdown" class="nav-item">
+            <NavDropdown title={t('loja.label')} id="basic-nav-dropdown" class="nav-item">
               <NavDropdown.Item className="dropdown-item" href="">Fardas</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">Livros</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">Diversos</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Contactos" id="basic-nav-dropdown" class="nav-item">
+            <NavDropdown title={t('contactos.label')} id="basic-nav-dropdown" class="nav-item">
               <NavDropdown.Item className="dropdown-item" href="">Queres ser Guia ?</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">Sede Nacional</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Form inline className="search-fields">
+          {/* <Form inline className="search-fields">
             <FontAwesomeIcon icon={faSearch} className="search-icon" />
-            <FormControl type="text" placeholder="Pesquisa..." className="mr-sm-2 search-input" />
-          </Form>
+            <FormControl type="text" placeholder={t('pesquisa.label')} className="mr-sm-2 search-input" />
+          </Form> */}
+          <div className="search-languages">
+            <Form inline className="search-fields">
+              <FontAwesomeIcon icon={faSearch} className="search-icon" />
+              <FormControl type="text" placeholder={t('pesquisa.label')} className="mr-sm-2 search-input" />
+            </Form>
+            <LanguageSelector />
+          </div>
         </Navbar.Collapse>
       </Navbar>
     </div>
