a26d0d6f29db55da72c3a9f3f84a1c74111a3f3b
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-05-28 09:55:19 +0100

    search icon added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './Header.css';
-import logoNegHeader from '../../assets/logo/logo_NEG_RGB.png';
-
 
 const Header = () => {
   return ( 
     <div>
       <Navbar fixed="top" expand="lg" className="navbar">
         <Navbar.Brand href="" className="header-logo">
-          {/* <img src={logoNegHeader} alt="logo" className="header-image"/> */}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="header-toggler"/>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="ml-auto">
             <NavDropdown title="Sobre" id="basic-nav-dropdown" class="nav-item">
               <NavDropdown.Item className="dropdown-item" href="">Associação</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">História do Guidismo</NavDropdown.Item>
@@ -44,9 +43,9 @@ const Header = () => {
               <NavDropdown.Item className="dropdown-item" href="">Sede Nacional</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Form inline>
-            {/* <label for="search-field"><i class="fa fa-search" style="margin-right: 10px;color: #154A7F;"></i></label> */}
-            <FormControl type="text" placeholder="Pesquisa..." className="mr-sm-2 search-control" />
+          <Form inline className="search-fields">
+            <FontAwesomeIcon icon={faSearch} className="search-icon"/>
+            <FormControl type="text" placeholder="Pesquisa..." className="mr-sm-2 search-input" />
           </Form>
         </Navbar.Collapse>
       </Navbar>
