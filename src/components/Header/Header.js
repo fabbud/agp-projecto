d0894d06b697f4cbda8f75ce93bb9b5236d6dafa
Margarida Sousa <margaridasousa@MBPdeMargarida2.home>
2020-05-30 11:41:13 +0100

    publicacoes submenu added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,13 +19,13 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="header-toggler" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <NavDropdown title={t('sobre.label')} id="basic-nav-dropdown" className="nav-item">
+            <NavDropdown title={t('sobre.label')} id="basic-nav-dropdown" className="nav-header">
               <NavDropdown.Item className="dropdown-item" href="">{t('associacao.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('historiaGuidismo.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('estatutos.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('associacaoMundial.label')}</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title={t('pedagogia.label')} id="basic-nav-dropdown" className="nav-item">
+            <NavDropdown title={t('pedagogia.label')} id="basic-nav-dropdown" className="nav-header">
               <NavDropdown.Item className="dropdown-item" href="">{t('metodoGuidista.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('ramoAvezinha.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('ramoAventura.label')}</NavDropdown.Item>
@@ -34,17 +34,22 @@ const Header = () => {
               <NavDropdown.Item className="dropdown-item" href="">{t('dirigente.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('palavrasPais.label')}</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title={t('publicacoes.label')} id="basic-nav-dropdown" className="nav-item">
+            <NavDropdown title={t('publicacoes.label')} id="basic-nav-dropdown" className="nav-header">
               <NavDropdown.Item className="dropdown-item" href="">{t('noticias.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('jornal.label')}</NavDropdown.Item>
-              <NavDropdown.Item className="dropdown-item" href="">{t('recursos.label')}</NavDropdown.Item>
+              <NavDropdown bsPrefix="dropdown-item" title={t('recursos.label')} className="dropright">
+                <NavDropdown.Item className="dropdown-item" href="">{t('livroEspecialidades.label')}</NavDropdown.Item>
+                <NavDropdown.Item className="dropdown-item" href="">{t('regulamentos.label')}</NavDropdown.Item>
+                <NavDropdown.Item className="dropdown-item" href="">{t('cartaoAssociada.label')}</NavDropdown.Item>
+                <NavDropdown.Item className="dropdown-item" href="">{t('ligacoesUteis.label')}</NavDropdown.Item>
+              </NavDropdown>
             </NavDropdown>
-            <NavDropdown title={t('loja.label')} id="basic-nav-dropdown" className="nav-item">
+            <NavDropdown title={t('loja.label')} id="basic-nav-dropdown" className="nav-header">
               <NavDropdown.Item className="dropdown-item" href="">{t('fardas.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('livros.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('diversos.label')}</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title={t('contactos.label')} id="basic-nav-dropdown" className="nav-item">
+            <NavDropdown title={t('contactos.label')} id="basic-nav-dropdown" className="nav-header">
               <NavDropdown.Item className="dropdown-item" href="">{t('queresSerGuia.label')}</NavDropdown.Item>
               <NavDropdown.Item className="dropdown-item" href="">{t('sedeNacional.label')}</NavDropdown.Item>
             </NavDropdown>
