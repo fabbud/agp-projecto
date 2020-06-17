936eba973cdb2124321f9e9197c384374f6c64a2
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-06-17 15:17:49 +0100

    router navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Suspense } from 'react';
 import './App.css';
-//import AssociacaoFront from './components/AssociacaoFront';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import './i18n';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
+import Homepage from './components/Homepage/Homepage';
 import AssociacaoFront from './components/AssociacaoFront';
 import RamosDirigente from './components/RamosDirigente/RamosDirigente';
 import AssociacaoMundial from './components/AssociacaoMundial/AssociacaoMundial';
@@ -15,14 +20,24 @@ import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-
-      <Suspense fallback={null}>
-        <Header />
-        <Noticias />
-        <Footer />
-      </Suspense>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/associacao" component={AssociacaoFront} />
+          <Route exact path="/ramos" component={RamosDirigente} />
+          <Route exact path="/associacao-mundial" component={AssociacaoMundial} />
+          <Route exact path="/palavra-pais" component={PalavraAosPais} />
+          <Route exact path="/ligações-úteis" component={LigacoesUteis} />
+          <Route exact path="/contactos" component={Contactos} />
+          <Route exact path="/noticias" component={Noticias} />
+        </Switch>
+        <Suspense fallback={null}>
+          <Header />
+          <Footer />
+        </Suspense>
+      </div>
+    </Router>
   );
 }
 
