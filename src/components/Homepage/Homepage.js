d7009a3ae8a1e164e28abaaa3771496d1c7d8d7d
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-06-30 15:57:24 +0100

    homepage done

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -11,63 +11,65 @@ import avezinhaLogo from '../../assets/images/Homepage/avezinha_white-8.png';
 import aventuraLogo from '../../assets/images/Homepage/aventura_white-8.png';
 import caravelaLogo from '../../assets/images/Homepage/caravela_white-8.png';
 import moinhoLogo from '../../assets/images/Homepage/moinho_white-8.png';
-
+import videoTeaser from '../../assets/videos/teaser_v_site.mp4';
 
 const Homepage = () => {
   const { t } = useTranslation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  });
+  }, []);
 
   return (
     <div className="Homepage">
-      <div className="">
-        <div className="home-video">Video</div>
-        <div className="home-ramos">
-          <div className="home-modelo-title">{t('homepage.tituloModeloPedagogico')}</div>
-          <div className="home-ramos-section">
-            <Link to="/pedagogia/ramos/avezinha">
-              <div className="home-ramos-card">
-                <img src={avezinhaFoto} className="home-ramos-img" alt="Ramo Avezinha" />
-                <div className="home-ramos-card-info avezinha-card">
-                  <img src={avezinhaLogo} className="home-ramos-card-logo" alt="Ramo Avezinha Símbolo" />
-                  <div className="home-ramos-card-title">{t('homepage.tituloAvezinha')}</div>
-                  <div className="home-ramos-card-text">{t('homepage.idadesAvezinha')}</div>
-                </div>
+      <div className="home-video">
+        <video controls controlsList="nodownload" autoPlay loop muted width="100%" height="100%">
+          <source src={videoTeaser} type="video/mp4" />
+        </video>
+      </div>
+      <div className="home-ramos">
+        <div className="home-modelo-title">{t('homepage.tituloModeloPedagogico')}</div>
+        <div className="home-ramos-section">
+          <Link to="/pedagogia/ramos/avezinha">
+            <div className="home-ramos-card">
+              <img src={avezinhaFoto} className="home-ramos-img" alt="Ramo Avezinha" />
+              <div className="home-ramos-card-info avezinha-card">
+                <img src={avezinhaLogo} className="home-ramos-card-logo" alt="Ramo Avezinha Símbolo" />
+                <div className="home-ramos-card-title">{t('homepage.tituloAvezinha')}</div>
+                <div className="home-ramos-card-text">{t('homepage.idadesAvezinha')}</div>
               </div>
-            </Link>
-            <Link to="/pedagogia/ramos/aventura">
-              <div className="home-ramos-card">
-                <img src={aventuraFoto} className="home-ramos-img" alt="Ramo Aventura" />
-                <div className="home-ramos-card-info aventura-card">
-                  <img src={aventuraLogo} className="home-ramos-card-logo" alt="Ramo Aventura Símbolo" />
-                  <div className="home-ramos-card-title">{t('homepage.tituloAventura')}</div>
-                  <div className="home-ramos-card-text">{t('homepage.idadesAventura')}</div>
-                </div>
+            </div>
+          </Link>
+          <Link to="/pedagogia/ramos/aventura">
+            <div className="home-ramos-card">
+              <img src={aventuraFoto} className="home-ramos-img" alt="Ramo Aventura" />
+              <div className="home-ramos-card-info aventura-card">
+                <img src={aventuraLogo} className="home-ramos-card-logo" alt="Ramo Aventura Símbolo" />
+                <div className="home-ramos-card-title">{t('homepage.tituloAventura')}</div>
+                <div className="home-ramos-card-text">{t('homepage.idadesAventura')}</div>
               </div>
-            </Link>
-            <Link to="/pedagogia/ramos/caravela">
-              <div className="home-ramos-card">
-                <img src={caravelaFoto} className="home-ramos-img" alt="Ramo Caravela" />
-                <div className="home-ramos-card-info caravela-card">
-                  <img src={caravelaLogo} className="home-ramos-card-logo" alt="Ramo Caravela Símbolo" />
-                  <div className="home-ramos-card-title">{t('homepage.tituloCaravela')}</div>
-                  <div className="home-ramos-card-text">{t('homepage.idadesCaravela')}</div>
-                </div>
+            </div>
+          </Link>
+          <Link to="/pedagogia/ramos/caravela">
+            <div className="home-ramos-card">
+              <img src={caravelaFoto} className="home-ramos-img" alt="Ramo Caravela" />
+              <div className="home-ramos-card-info caravela-card">
+                <img src={caravelaLogo} className="home-ramos-card-logo" alt="Ramo Caravela Símbolo" />
+                <div className="home-ramos-card-title">{t('homepage.tituloCaravela')}</div>
+                <div className="home-ramos-card-text">{t('homepage.idadesCaravela')}</div>
               </div>
-            </Link>
-            <Link to="/pedagogia/ramos/moinho">
-              <div className="home-ramos-card">
-                <img src={moinhoFoto} className="home-ramos-img" alt="Ramo Moinho" />
-                <div className="home-ramos-card-info moinho-card">
-                  <img src={moinhoLogo} className="home-ramos-card-logo" alt="Ramo Moinho Símbolo" />
-                  <div className="home-ramos-card-title">{t('homepage.tituloMoinho')}</div>
-                  <div className="home-ramos-card-text">{t('homepage.idadesMoinho')}</div>
-                </div>
+            </div>
+          </Link>
+          <Link to="/pedagogia/ramos/moinho">
+            <div className="home-ramos-card">
+              <img src={moinhoFoto} className="home-ramos-img" alt="Ramo Moinho" />
+              <div className="home-ramos-card-info moinho-card">
+                <img src={moinhoLogo} className="home-ramos-card-logo" alt="Ramo Moinho Símbolo" />
+                <div className="home-ramos-card-title">{t('homepage.tituloMoinho')}</div>
+                <div className="home-ramos-card-text">{t('homepage.idadesMoinho')}</div>
               </div>
-            </Link>
-          </div>
+            </div>
+          </Link>
         </div>
       </div>
       <div className="home-section-palavras">
@@ -110,18 +112,18 @@ const Homepage = () => {
 
       <div className="home-noticias">
         <div className="home-noticia-card">
-          <div className="home-noticia-title">O Trevo também fica em casa</div>
-          <div className="home-noticia-text">Enquanto não sai para as ruas, o jornal O Trevo sai em casa. Versão digital já disponível. </div>
+          <div className="home-noticia-title">{t('homepage.tituloNoticia1')}</div>
+          <div className="home-noticia-text">{t('homepage.textoNoticia1')}</div>
           <button type="submit" className="home-button">{t('buttons.lerMais')}</button>
         </div>
         <div className="home-noticia-card">
-          <div className="home-noticia-title">DICAS PARA TE MANTERES LIGADA ÀS TUAS AMIGAS GUIAS</div>
-          <div className="home-noticia-text">Descobre algumas ideias para pores em prática com a tua Patrulha em... casa!</div>
+          <div className="home-noticia-title">{t('homepage.tituloNoticia2')}</div>
+          <div className="home-noticia-text">{t('homepage.textoNoticia2')}</div>
           <button type="submit" className="home-button">{t('buttons.lerMais')}</button>
         </div>
         <div className="home-noticia-card">
-          <div className="home-noticia-title">PANDEMIA POR SURTO DE CORONAVÍRUS</div>
-          <div className="home-noticia-text">Atividades Guidistas presenciais suspensas pela necessidade do isolamento social.</div>
+          <div className="home-noticia-title">{t('homepage.tituloNoticia3')}</div>
+          <div className="home-noticia-text">{t('homepage.textoNoticia3')}</div>
           <button type="submit" className="home-button">{t('buttons.lerMais')}</button>
         </div>
       </div>
