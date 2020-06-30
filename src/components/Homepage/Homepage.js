5af5e003355f93e6812efff2dd52fd63966074e0
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-06-30 11:31:56 +0100

    logos white

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import ReactHtmlParser from 'react-html-parser';
 import './Homepage.css';
 import avezinhaFoto from '../../assets/images/Homepage/FotoAvezinha.jpg';
 import aventuraFoto from '../../assets/images/Homepage/FotoAventura.jpg';
 import caravelaFoto from '../../assets/images/Homepage/FotoCaravela.jpg';
 import moinhoFoto from '../../assets/images/Homepage/FotoMoinho.jpg';
-import avezinhaLogo from '../../assets/images/Homepage/LogoAvezinha.png';
-import aventuraLogo from '../../assets/images/Homepage/LogoAventura.png';
-import caravelaLogo from '../../assets/images/Homepage/LogoCaravela.png';
-import moinhoLogo from '../../assets/images/Homepage/LogoMoinho.png';
+import avezinhaLogo from '../../assets/images/Homepage/avezinha_white-8.png';
+import aventuraLogo from '../../assets/images/Homepage/aventura_white-8.png';
+import caravelaLogo from '../../assets/images/Homepage/caravela_white-8.png';
+import moinhoLogo from '../../assets/images/Homepage/moinho_white-8.png';
 
 
 const Homepage = () => {
@@ -24,7 +25,7 @@ const Homepage = () => {
       <div className="">
         <div className="home-video">Video</div>
         <div className="home-ramos">
-          <div className="home-modelo-title">Modelo Pedagógico</div>
+          <div className="home-modelo-title">{t('homepage.tituloModeloPedagogico')}</div>
           <div className="home-ramos-section">
             <Link to="/pedagogia/ramos/avezinha">
               <div className="home-ramos-card">
@@ -72,7 +73,7 @@ const Homepage = () => {
       <div className="home-section-palavras">
         <div className="home-section-side">
           <div className="home-section-center">
-            <div className="home-section-title">Palavra aos pais</div>
+            <div className="home-section-title">{t('homepage.tituloPalavraPais')}</div>
             <div className="home-section-subtitle">Ter uma Filha nas Guias, porque sim?</div>
             <div className="home-section-text">
               Nas Guias ajudamos as crianças e jovens a desenvolverem plenamente o seu potencial
@@ -91,7 +92,7 @@ const Homepage = () => {
         <div className="home-section-side home-jornal-foto" />
         <div className="home-section-side">
           <div className="home-section-center">
-            <div className="home-section-title">Jornal &quot;O Trevo&quot;</div>
+            <div className="home-section-title">{ReactHtmlParser(t('homepage.tituloOjornal'))}</div>
             <div className="home-section-subtitle">&quot;Eu e o Outro: despertar para uma relação positiva&quot;</div>
             <div className="home-section-text">
               Jornal Oficial da Associação
@@ -107,23 +108,23 @@ const Homepage = () => {
         </div>
       </div>
 
-      {/* <div className="home-noticias">
+      <div className="home-noticias">
         <div className="home-noticia-card">
-          <div className="home-noticia-title">Noticia 1</div>
-          <div className="home-noticia-text">Texto 1</div>
+          <div className="home-noticia-title">O Trevo também fica em casa</div>
+          <div className="home-noticia-text">Enquanto não sai para as ruas, o jornal O Trevo sai em casa. Versão digital já disponível. </div>
           <button type="submit" className="home-button">{t('buttons.lerMais')}</button>
         </div>
         <div className="home-noticia-card">
-          <div className="home-noticia-title">Noticia 2</div>
-          <div className="home-noticia-text">Texto 2</div>
+          <div className="home-noticia-title">DICAS PARA TE MANTERES LIGADA ÀS TUAS AMIGAS GUIAS</div>
+          <div className="home-noticia-text">Descobre algumas ideias para pores em prática com a tua Patrulha em... casa!</div>
           <button type="submit" className="home-button">{t('buttons.lerMais')}</button>
         </div>
         <div className="home-noticia-card">
-          <div className="home-noticia-title">Noticia 3</div>
-          <div className="home-noticia-text">Texto 3</div>
+          <div className="home-noticia-title">PANDEMIA POR SURTO DE CORONAVÍRUS</div>
+          <div className="home-noticia-text">Atividades Guidistas presenciais suspensas pela necessidade do isolamento social.</div>
           <button type="submit" className="home-button">{t('buttons.lerMais')}</button>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
