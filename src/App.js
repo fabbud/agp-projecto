ee878cafe19eee5a99ed787da4c481767a92c54b
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-07-02 18:44:22 +0100

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { Suspense } from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./i18n";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header/Header";
-import Homepage from "./components/Homepage/Homepage";
-import RamosDirigente from "./components/RamosDirigente/RamosDirigente";
-import AssociacaoMundial from "./components/AssociacaoMundial/AssociacaoMundial";
-import PalavraAosPais from "./components/PalavraAosPais/PalavraAosPais";
-import Noticias from "./components/Noticias/Noticias";
-import LigacoesUteis from "./components/LigacoesUteis/LigacoesUteis";
-import Contactos from "./components/Contactos/Contactos";
-import Footer from "./components/Footer/Footer";
-import AssociacaoFront from "./components/Associacao/AssociacaoFront";
-//import LojaMaster from "./components/Loja/LojaMaster";
-import ConteudoNoticia from "./components/ConteudoNoticia/ConteudoNotica";
+import React, { Suspense } from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import './i18n';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import Homepage from './components/Homepage/Homepage';
+import RamosDirigente from './components/RamosDirigente/RamosDirigente';
+import AssociacaoMundial from './components/AssociacaoMundial/AssociacaoMundial';
+import PalavraAosPais from './components/PalavraAosPais/PalavraAosPais';
+import Noticias from './components/Noticias/Noticias';
+import LigacoesUteis from './components/LigacoesUteis/LigacoesUteis';
+import Contactos from './components/Contactos/Contactos';
+import Footer from './components/Footer/Footer';
+import AssociacaoFront from './components/Associacao/AssociacaoFront';
+import LojaMaster from './components/Loja/LojaMaster';
+import ConteudoNoticia from './components/ConteudoNoticia/ConteudoNotica';
+
 
 function App() {
   return (
@@ -24,38 +29,15 @@ function App() {
           <Switch>
             <Route exact path="/" component={Homepage} />
             <Route exact path="/sobre/associacao" component={AssociacaoFront} />
-            <Route
-              exact
-              path="/sobre/associacao-mundial"
-              component={AssociacaoMundial}
-            />
-            <Route
-              exact
-              path="/pedagogia/ramos/:tipo"
-              component={RamosDirigente}
-            />
-            <Route
-              exact
-              path="/pedagogia/dirigente"
-              component={RamosDirigente}
-            />
-            <Route
-              exact
-              path="/pedagogia/palavra-pais"
-              component={PalavraAosPais}
-            />
-            <Route
-              exact
-              path="/publicações/recursos/ligações-úteis"
-              component={LigacoesUteis}
-            />
-            <Route exact path="/contactos/:modo" component={Contactos} />
+            <Route exact path="/sobre/associacao-mundial" component={AssociacaoMundial} />
+            <Route exact path="/pedagogia/ramos/:tipo" component={RamosDirigente} />
+            <Route exact path="/pedagogia/dirigente" component={RamosDirigente} />
+            <Route exact path="/pedagogia/palavra-pais" component={PalavraAosPais} />
             <Route exact path="/publicações/noticias" component={Noticias} />
-            <Route
-              exact
-              path="/publicações/noticias/:id"
-              component={ConteudoNoticia}
-            />
+            <Route exact path="/publicações/noticias/:id" component={ConteudoNoticia} />
+            <Route exact path="/publicações/recursos/ligações-úteis" component={LigacoesUteis} />
+            <Route exact path="/contactos/:modo" component={Contactos} />
+            <Route exact path="/loja/:modo" component={LojaMaster} />
           </Switch>
           <Header />
           <Footer />
