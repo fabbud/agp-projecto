0f186b1847ab0b785181c815cbefed8db8bf8685
Antonio Branco <antoniobranco@sapo.pt>
2020-07-07 15:52:54 +0100

    Jornal pdf

--- src/components/Jornal/JornalCapa.js
+++ src/components/Jornal/JornalCapa.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Jornal.css'
+import trevo from '../../assets/pdfs/Jornal/TREVO_27.pdf';
 
 
 
@@ -13,12 +14,18 @@ function JornalCapa(props) {
         <div className='cardJornal'>
             <h4 className='titulo'>{props.titulo}</h4>
             <h4 className='anoEdicao'>{`${props.ano} - ${props.edicao}ª edição`}</h4>
-            <img className='capa'
-                alt={props.alt}
-                src={require(`../../assets/images/Jornal/${props.src}`)}
-            />
-            {/* <div class="download"><a href={`../../assets/pdfs/Jornal/${props.pdf}`}> */}
-            <button class='download'></button>
+            <div className='capaDownload'>
+                <img className='capa'
+                    alt={props.alt}
+                    src={require(`../../assets/images/Jornal/${props.src}`)}
+                />
+                <a className='download' href={`../../assets/pdfs/Jornal/${props.pdf}`} download>
+                    <img className='downloadImage' alt={'download Jornal O Trevo Guias de Portugal'}
+                        src={require('../../assets/images/Outros/download.jpg')}
+                    />
+                </a>
+            </div>
+
             <div className='temasBox'>
                 {props.temas.map((tema) => {
                     return <p className='temas'>- {tema}</p>
