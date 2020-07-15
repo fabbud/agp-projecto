1e27d52ad053a4c8ffaa605d6f096a8593693329
Antonio Branco <antoniobranco@sapo.pt>
2020-07-15 18:47:38 +0100

    final version journal backoffice

--- src/components/Backoffice/Journal/Journal.js
+++ src/components/Backoffice/Journal/Journal.js
@@ -4,27 +4,45 @@ class Journal extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            journal: {
-                publish: false,
-                edition: '',
-                year: '',
-                pt_title: '',
-                en_title: '',
-                thumbnail: '',
-                pdf_link: '',
-                pt_intro_text_1: '',
-                pt_intro_text_2: '',
-                pt_intro_text_3: '',
-                pt_intro_text_4: '',
-                pt_intro_text_5: '',
-                en_intro_text_1: '',
-                en_intro_text_2: '',
-                en_intro_text_3: '',
-                en_intro_text_4: '',
-                en_intro_text_5: '',
-                flash: ''
-            }
+            publish: false,
+            edition: '',
+            year: '',
+            pt_title: '',
+            en_title: '',
+            thumbnail: '',
+            pdf_link: '',
+            pt_intro_text_1: '',
+            pt_intro_text_2: '',
+            pt_intro_text_3: '',
+            pt_intro_text_4: '',
+            pt_intro_text_5: '',
+            en_intro_text_1: '',
+            en_intro_text_2: '',
+            en_intro_text_3: '',
+            en_intro_text_4: '',
+            en_intro_text_5: '',
+            flash: ''
 
+            // journal: {
+            //     publish: false,
+            //     edition: '',
+            //     year: '',
+            //     pt_title: '',
+            //     en_title: '',
+            //     thumbnail: '',
+            //     pdf_link: '',
+            //     pt_intro_text_1: '',
+            //     pt_intro_text_2: '',
+            //     pt_intro_text_3: '',
+            //     pt_intro_text_4: '',
+            //     pt_intro_text_5: '',
+            //     en_intro_text_1: '',
+            //     en_intro_text_2: '',
+            //     en_intro_text_3: '',
+            //     en_intro_text_4: '',
+            //     en_intro_text_5: '',
+            //     flash: ''
+            // }
         }
     }
 
@@ -45,36 +63,49 @@ class Journal extends React.Component {
                 .then(res => res.json())
                 .then(res => {
                     this.setState({
-                        journal: res[0]
+                        edition: res[0].edition,
+                        year: res[0].year,
+                        pt_title: res[0].pt_title,
+                        en_title: res[0].en_title,
+                        thumbnail: res[0].thumbnail,
+                        pdf_link: res[0].pdf_link,
+                        pt_intro_text_1: res[0].pt_intro_text_1,
+                        pt_intro_text_2: res[0].pt_intro_text_2,
+                        pt_intro_text_3: res[0].pt_intro_text_3,
+                        pt_intro_text_4: res[0].pt_intro_text_4,
+                        pt_intro_text_5: res[0].pt_intro_text_5,
+                        en_intro_text_1: res[0].en_intro_text_1,
+                        en_intro_text_2: res[0].en_intro_text_2,
+                        en_intro_text_3: res[0].en_intro_text_3,
+                        en_intro_text_4: res[0].en_intro_text_4,
+                        en_intro_text_5: res[0].en_intro_text_5,
+                        publish: res[0].publish,
                     })
                 })
         }
     }
 
     updateField = (event) => {
+        event.preventDefault();
         let name = event.target.name
         let value = event.target.value
         this.setState({ [name]: value })
-        console.log(name)
-        console.log(value)
     }
 
     updatePublish = () => {
-        const publish = this.state.journal.publish;
+        const publish = this.state.publish;
         this.setState({ publish: !publish });
     }
 
-
-    
-
     handleSubmit = (event) => {
         event.preventDefault()
-        const { flash, ...newEdition } = this.state.journal
+        const { flash, ...newEdition } = this.state
         const { match } = this.props;
         let getEdition = match.params.edition;
         console.log(getEdition)
         if (getEdition) {
-            fetch("/editPublication",
+            console.log(newEdition)
+            fetch("/journal/editPublication",
                 {
                     method: 'PUT',
                     headers: new Headers({
@@ -88,10 +119,10 @@ class Journal extends React.Component {
                     res => this.setState({ "flash": res.flash }),
                     err => this.setState({ "flash": err.flash })
                 )
-            console.log(JSON.stringify(this.state.journal.flash))
-            console.log(newEdition)
+            //console.log(JSON.stringify(this.state.journal.flash))
         } else {
-            fetch("/publish",
+            console.log(newEdition)
+            fetch("/journal/publish",
                 {
                     method: 'POST',
                     headers: new Headers({
@@ -104,15 +135,11 @@ class Journal extends React.Component {
                     res => this.setState({ "flash": res.flash }),
                     err => this.setState({ "flash": err.flash })
                 )
-            console.log(JSON.stringify(this.state.journal.flash))
-
+            //console.log(JSON.stringify(this.state.journal.flash))
         }
     }
 
-
     render() {
-
-
         const {
             publish,
             edition,
@@ -131,8 +158,9 @@ class Journal extends React.Component {
             en_intro_text_3,
             en_intro_text_4,
             en_intro_text_5
-        } = this.state.journal;
-        console.log(this.state.journal)
+        } = this.state;
+
+        console.log("pdf final", pdf_link)
 
         return (
             <div>
@@ -140,39 +168,39 @@ class Journal extends React.Component {
 
                 <form onSubmit={this.handleSubmit}>
                     <div>Número Edição:</div>
-                    <input type='text' name='journal.edition' defaultValue={edition} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='edition' value={edition} onChange={(event) => this.updateField(event)} disabled />
                     <div>Título Pt:</div>
-                    <input type='text' name='journal.pt_title' defaultValue={pt_title} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='pt_title' value={pt_title} onChange={(event) => this.updateField(event)} />
                     <div>Título En:</div>
-                    <input type='text' name='journal.en_title' vadefaultValuelue={en_title} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='en_title' value={en_title} onChange={(event) => this.updateField(event)} />
                     <div>Ano:</div>
-                    <input type='text' name='journal.year' valdefaultValueue={year} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='year' value={year} onChange={(event) => this.updateField(event)} />
                     <div>Imagem:</div>
-                    <input type='text' name='journal.thumbnail' defaultValue={thumbnail} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='thumbnail' value={thumbnail} onChange={(event) => this.updateField(event)} />
                     <div>Pdf:</div>
-                    <input type='text' name='journal.pdf_link' defaultValue={pdf_link} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='pdf_link' value={pdf_link} onChange={(event) => this.updateField(event)} />
                     <div>Título 1 Pt:</div>
-                    <input type='text' name='journal.pt_intro_text_1' defaultValue={pt_intro_text_1} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='pt_intro_text_1' value={pt_intro_text_1} onChange={(event) => this.updateField(event)} />
                     <div>Título 2 Pt:</div>
-                    <input type='text' name='journal.pt_intro_text_2' defaultValue={pt_intro_text_2} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='pt_intro_text_2' value={pt_intro_text_2} onChange={(event) => this.updateField(event)} />
                     <div>Título 3 Pt:</div>
-                    <input type='text' name='journal.pt_intro_text_3' defaultValue={pt_intro_text_3} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='pt_intro_text_3' value={pt_intro_text_3} onChange={(event) => this.updateField(event)} />
                     <div>Título 4 Pt:</div>
-                    <input type='text' name='journal.pt_intro_text_4' defaultValue={pt_intro_text_4} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='pt_intro_text_4' value={pt_intro_text_4} onChange={(event) => this.updateField(event)} />
                     <div>Título 5 Pt:</div>
-                    <input type='text' name='journal.pt_intro_text_5' defaultValue={pt_intro_text_5} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='pt_intro_text_5' value={pt_intro_text_5} onChange={(event) => this.updateField(event)} />
                     <div>Título 1 En:</div>
-                    <input type='text' name='journal.en_intro_text_1' defaultValue={en_intro_text_1} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='en_intro_text_1' value={en_intro_text_1} onChange={(event) => this.updateField(event)} />
                     <div>Título 2 En:</div>
-                    <input type='text' name='journal.en_intro_text_2' defaultValue={en_intro_text_2} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='en_intro_text_2' value={en_intro_text_2} onChange={(event) => this.updateField(event)} />
                     <div>Título 3 En:</div>
-                    <input type='text' name='journal.en_intro_text_3' defaultValue={en_intro_text_3} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='en_intro_text_3' value={en_intro_text_3} onChange={(event) => this.updateField(event)} />
                     <div>Título 4 En:</div>
-                    <input type='text' name='journal.en_intro_text_4' defaultValue={en_intro_text_4} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='en_intro_text_4' value={en_intro_text_4} onChange={(event) => this.updateField(event)} />
                     <div>Título 5 En:</div>
-                    <input type='text' name='journal.en_intro_text_5' defaultValue={en_intro_text_5} onChange={(event) => this.updateField(event)} />
+                    <input type='text' name='en_intro_text_5' value={en_intro_text_5} onChange={(event) => this.updateField(event)} />
                     <div>Publicar</div>
-                    <input type='checkbox' name='publish' defaultValue={publish} onChange={this.updatePublish} />
+                    <input type='checkbox' name='publish' value={publish} onChange={this.updatePublish} />
                     <button type='submit'>ENVIAR</button>
                 </form>
             </div>
